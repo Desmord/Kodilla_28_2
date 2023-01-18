5b8abade2c2c2d1a2198f606972dfe881e8ffb22
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-01-18 12:28:02 +0100

    Etap 1,2,3 zrobiony. W trakcie epatu 4

---
+++ client/app.js
@@ -0,0 +1,25 @@
+const loginForm = document.querySelector(`#welcome-form`);
+const messagesSection = document.querySelector(`#messages-section`);
+const messageList = document.querySelector(`#messages-list`);
+const addMessageForm = document.querySelector(`#add-messages-form`);
+const userNameInput = document.querySelector(`#username`);
+const messageContentInput = document.querySelector(`#message-content`);
+
+let userName = ``;
+
+
+const login = (e) => {
+
+    e.preventDefault();
+
+    // klasa show
+
+
+}
+
+
+loginForm.addEventListener(`submit`, login);
+
+
+console.log(`witam`)
+console.log(loginForm)
